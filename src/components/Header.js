9507c8e42fda23b5220696ac4bd644964e182e83
JJLindsey <jlindsey010@gmail.com>
2022-04-08 17:13:20 -0700

    add inlinestyling for toolbar margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import { Typography } from '@material-ui/core';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import {makeStyles} from '@material-ui/core/styles'
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -17,17 +18,29 @@ function ElevationScroll(props) {
     });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    //using spread operator we have copied all styles over from toolbar object
+    ...theme.mixins.toolbar
+  }
+}))
+
 
 export default function Header(props) {
+  const classes = useStyles()
+
   return (
-    <ElevationScroll>
-        <AppBar position='fixed' color='primary'>
-            <Toolbar>
-              <Typography variant='h5' color='secondary'>
-                Navigation
-              </Typography>
-            </Toolbar>
-        </AppBar>
-    </ElevationScroll> 
+    <React.Fragment>
+      <ElevationScroll>
+          <AppBar position='fixed' color='primary'>
+              <Toolbar>
+                <Typography variant='h5' color='secondary'>
+                  Navigation
+                </Typography>
+              </Toolbar>
+          </AppBar>
+      </ElevationScroll> 
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   )
 }
